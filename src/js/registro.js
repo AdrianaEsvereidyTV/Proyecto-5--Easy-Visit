3cde22645198b20ed7d6d7373a6fcc2f647eb200
Adriana <nossa.musica.07.10@gmail.com>
2018-08-13 19:18:49 -0500

    Asignando dirección a la cámara

--- src/js/registro.js
+++ src/js/registro.js
@@ -1,36 +1,39 @@
 btnRegistrar.addEventListener('click', e => {
+  location.href = '../views/camera.html';
   let name = document.getElementById('name').value;
   let email = document.getElementById('email').value;
   let personVisit = document.getElementById('personVisit').value;
   let company = document.getElementById('company').value;
 
-  if (name === ''){
+  if (name === '') {
     alert('Ingresa el nombre completo');
     return false;
-  } if (email === ''){
+  }
+  if (email === '') {
     alert('Ingresa el correo electrónico');
     return false;
-  } if (personVisit === ''){
+  }
+  if (personVisit === '') {
     alert('Ingresa tu anfitrion');
     return false;
-  } if (company === '') {
+  }
+  if (company === '') {
     alert('Ingresa la empresa a visitar');
     return false;
   } else {
     db.collection('visitors').add({
-        createdAt: firebase.firestore.FieldValue.serverTimestamp(),
-        name: name,
-        mail: email,
-        personVisit: personVisit,
-        company: company
-      })
+      createdAt: firebase.firestore.FieldValue.serverTimestamp(),
+      name: name,
+      mail: email,
+      personVisit: personVisit,
+      company: company
+    })
       .then(function (docRef) {
         console.log('Document written with ID: ', docRef.id);
         document.getElementById('name').value = '';
         document.getElementById('email').value = '';
         document.getElementById('personVisit').value = '';
         document.getElementById('company').value = '';
-
       })
       .catch(function (error) {
 
